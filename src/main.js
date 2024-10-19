fbb1b1a6b8d5b2f776260d5c5307533694e3e591
daria_hk <honcharuk.daria95@gmail.com>
2024-10-19 23:40:25 +0200

    add filles

--- src/main.js
+++ src/main.js
@@ -30,8 +30,6 @@ async function handleSearch(evt) {
   try {
     const data = await getImg(searchValue);
 
-    console.log(data);
-
     if (data.total === 0) {
       imgs.innerHTML = "";
       notFoundTextEl.innerHTML = `Results for query <span>${searchValue}</span> not found!`;
@@ -71,48 +69,4 @@ async function handleSearch(evt) {
       console.log("completed");
       loaderClass.style.display = "none";
   };
-/*
-  getImg(searchValue)
-    .then((data) => {
-      if (data.total === 0) {
-        imgs.innerHTML = "";
-        notFoundTextEl.innerHTML = `Results for query <span>${searchValue}</span> not found!`;
-        iziToast.show({
-          title: "Error",
-          message:
-            "Sorry, there are no images matching your search query. Please try again!",
-          backgroundColor: "#B51B1B",
-          messageColor: "white",
-          titleColor: "yellow",
-        });
-        return;
-      }
-
-      createCardsMarkup(data.hits);
-
-      const imageElements = document.querySelectorAll(".gallery-img");
-      const loadPromises = Array.from(imageElements).map((img) => {
-        return new Promise((resolve) => {
-          img.onload = resolve;
-          img.onerror = resolve;
-        });
-      });
-
-      Promise.all(loadPromises)
-        .then(() => {
-          console.log(loadPromises);
-        })
-        .catch((err) => {
-          loaderClass.innerHTML = "";
-          console.error(err);
-        });
-    })
-    .catch(console.error)
-    .finally(() => {
-      console.log("completed");
-      loaderClass.style.display = "none";
-    });
-  searchInput.value = "";
-
-  */
 }
